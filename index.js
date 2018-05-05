5a71b705bfa978ff8423b50408f8ad0afea279c3
Amir Sohi <amirasohi@gmail.com>
2018-05-05 12:54:47 +0100

    App runs successfully

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.plugin(require('mongoose-unique-validator'));
+mongoose.Promise = require('bluebird');
+const router = require('./config/router');
+// const errorHandler = require('./lib/errorHandler');
+
+const { dbURI, port } = require('./config/environment');
 
 const app = express();
-const port = 4000;
 app.use(express.static(`${__dirname}/public`));
+
+mongoose.connect(dbURI);
+app.use(bodyParser.json());
+
+app.use('/api', router);
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 
